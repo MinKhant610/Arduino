2bc196b6d52de0f8114f9bd983614bd45122fa55
MinKhant610 <minkhantiopp@gmail.com>
2022-11-17 23:46:59 +0630

    just update

--- IR_reciever/IR_reciever.ino
+++ IR_reciever/IR_reciever.ino
@@ -19,19 +19,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (ir_rev.decode(&results)){
     Serial.println(results.value, HEX);
-    ir_rev.resume();
-    
-    if (results.value == 0xFF30CF){
+    ir_rev.resume(); 
+  }
+  if (results.value == 0xFF30CF){
       digitalWrite(10, HIGH);
-    }else if (results.value = 0xFF18E7){
+    }else if (results.value == 0xFF18E7){
       digitalWrite(9, HIGH);
-    }else if (results.value = 0xFF7A85){
+    }else if (results.value == 0xFF7A85){
       digitalWrite(8, HIGH);
-    }else if (results.value = 0xFF6897){
-      for(int i = 8; i < 11; i++){
+    }else if (results.value == 0xFF6897){
+      for (int i = 8; i < 11; i++){
         digitalWrite(i, LOW);
       }
     }
-  }
   delay(1000);
 }
